1007482cc7b620eab72b6534ea7642f583c1a192
Lucas Candido <lucagamerbr07@gmail.com>
2025-12-08 01:58:54 +0000

    File changes

--- src/pages/RelatoriosFinanceiros.jsx
+++ src/pages/RelatoriosFinanceiros.jsx
@@ -857,7 +857,7 @@ export default function RelatoriosFinanceirosPage() {
 
       {/* Dialog Criar Vendedor */}
       <Dialog open={dialogVendedorAberto} onOpenChange={setDialogVendedorAberto}>
-        <DialogContent className="max-w-md">
+        <DialogContent className="max-w-md max-h-[90vh] overflow-y-auto">
           <DialogHeader>
             <DialogTitle>Criar Novo Vendedor</DialogTitle>
           </DialogHeader>
