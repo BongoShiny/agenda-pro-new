29c46f3c7141db02d4bb25a9e0191c92eb4edc47
Lucas Candido <lucagamerbr07@gmail.com>
2026-01-14 04:45:01 +0000

    File changes

--- src/pages/ConfiguracaoTerapeutas.jsx
+++ src/pages/ConfiguracaoTerapeutas.jsx
@@ -667,8 +667,8 @@ export default function ConfiguracaoTerapeutasPage() {
             const configs = getConfiguracoesUnidade(unidade.id);
             const disponiveis = getProfissionaisDisponiveis(unidade.id);
 
-            return (
-              <TabsContent key={unidade.id} value={unidade.id}>
+            return usuarioAtual?.cargo === "gerencia_unidades" ? (
+              <div key={unidade.id}>
                 <Card>
                   <CardHeader>
                     <CardTitle className="flex items-center justify-between">
